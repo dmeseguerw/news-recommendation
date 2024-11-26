b4c863d0684f3d206f91c55c1cb84d500020b9a0
dmeseguerw <daniel@meseguer.biz>
2024-11-26 20:04:17 +0100

    Model working but failing in loss

--- src/ebrec/models/newsrec_pytorch/NRMSModel.py
+++ src/ebrec/models/newsrec_pytorch/NRMSModel.py
@@ -7,19 +7,21 @@ class NRMSModel(nn.Module):
     def __init__(self, hparams_nrms, word2vec_embedding, seed):
         super(NRMSModel, self).__init__()
         self.news_encoder = NewsEncoder(hparams_nrms, word2vec_embedding, seed)
+        self.candidate_encoder = NewsEncoder(hparams_nrms, word2vec_embedding, seed)
         self.user_encoder = UserEncoder(self.news_encoder, hparams_nrms, seed)
-        print(self.news_encoder)
-        print(self.user_encoder)
+        print("News encoder: ", self.news_encoder)
+        print("User encoder: ", self.user_encoder)
         self.hparams_nrms = hparams_nrms
     
     def forward(self, his_input_title, pred_input_title):
-        news_present = torch.stack([self.news_encoder(title) for title in pred_input_title], dim=1)
+        _, _, title_size = pred_input_title.shape
         user_present = self.user_encoder(his_input_title)
-        print(news_present.shape)
-        print(user_present.shape)
+        pred_input_title_flat = pred_input_title.view(-1, title_size)
+        news_present = self.candidate_encoder(pred_input_title_flat)
         
-        # Calculate inner product
-        preds = torch.matmul(news_present.transpose, user_present)
-        
-        preds = torch.softmax(preds, dim=-1)
+        # apply inner product between user_present and news_present
+        preds = torch.matmul(user_present, news_present.t())
+        # apply softmax to get probability
+        preds = torch.softmax(preds, dim=1)
+
         return preds
