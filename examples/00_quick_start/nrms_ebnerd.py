db437148401cd6becb18125d3c4bbb96a68db265
dmeseguerw <daniel@meseguer.biz>
2024-11-28 14:30:22 +0100

    Adding validation loop, loss function working but run out of memory when running

--- examples/00_quick_start/nrms_ebnerd.py
+++ examples/00_quick_start/nrms_ebnerd.py
@@ -1,10 +1,18 @@
+# %% [markdown]
+# # Getting started
+# 
+# In this notebook, we illustrate how to use the Neural News Recommendation with Multi-Head Self-Attention ([NRMS](https://aclanthology.org/D19-1671/)). The implementation is taken from the [recommenders](https://github.com/recommenders-team/recommenders) repository. We have simply stripped the model to keep it cleaner.
+# 
+# We use a small dataset, which is downloaded from [recsys.eb.dk](https://recsys.eb.dk/). All the datasets are stored in the folder path ```~/ebnerd_data/*```.
+
+# %% [markdown]
+# ## Load functionality
+
+# %%
 from transformers import AutoTokenizer, AutoModel
 from pathlib import Path
 import tensorflow as tf
-import datetime as dt
 import polars as pl
-import gc
-import os
 
 from ebrec.utils._constants import (
     DEFAULT_HISTORY_ARTICLE_ID_COL,
@@ -31,15 +39,14 @@ from ebrec.utils._articles import create_article_id_to_value_mapping
 from ebrec.utils._nlp import get_transformers_word_embeddings
 from ebrec.utils._python import write_submission_file, rank_predictions_by_score
 
-from ebrec.models.newsrec.dataloader import NRMSDataLoader
-from ebrec.models.newsrec.model_config import hparams_nrms
-from ebrec.models.newsrec import NRMSModel
+from ebrec.models.newsrec_pytorch.dataloader import NRMSDataLoader
+from ebrec.models.newsrec_pytorch.model_config import hparams_nrms
+from ebrec.models.newsrec_pytorch import NRMSModel
 
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-# conda activate ./venv/
-# python examples/00_quick_start/nrms_ebnerd.py
+# %% [markdown]
+# ## Load dataset
 
-
+# %%
 def ebnerd_from_path(path: Path, history_size: int = 30) -> pl.DataFrame:
     """
     Load ebnerd - function
@@ -67,21 +74,24 @@ def ebnerd_from_path(path: Path, history_size: int = 30) -> pl.DataFrame:
     )
     return df_behaviors
 
+# %% [markdown]
+# ### Generate labels
+# We sample a few just to get started. For testset we just make up a dummy column with 0 and 1 - this is not the true labels.
 
+# %%
 PATH = Path("~/ebnerd_data").expanduser()
-DUMP_DIR = PATH.joinpath("ebnerd_predictions")
+print(PATH)
+DATASPLIT = "ebnerd_demo"
+DUMP_DIR = PATH.joinpath("downloads1")
 DUMP_DIR.mkdir(exist_ok=True, parents=True)
-DT_NOW = dt.datetime.now()
 
-MODEL_NAME = f"NRMS-{DT_NOW}"
-MODEL_WEIGHTS = DUMP_DIR.joinpath(f"state_dict/{MODEL_NAME}/weights")
-LOG_DIR = DUMP_DIR.joinpath(f"runs/{MODEL_NAME}")
+# %% [markdown]
+# In this example we sample the dataset, just to keep it smaller. Also, one can simply add the testset similary to the validation.
 
-DATASPLIT = "ebnerd_small"
-HISTORY_SIZE = 20
-FRACTION = 1.0
-EPOCHS = 5
+# %% [markdown]
+# 
 
+# %%
 COLUMNS = [
     DEFAULT_USER_COL,
     DEFAULT_HISTORY_ARTICLE_ID_COL,
@@ -89,10 +99,11 @@ COLUMNS = [
     DEFAULT_CLICKED_ARTICLES_COL,
     DEFAULT_IMPRESSION_ID_COL,
 ]
+HISTORY_SIZE = 10
+FRACTION = 0.01
 
 df_train = (
     ebnerd_from_path(PATH.joinpath(DATASPLIT, "train"), history_size=HISTORY_SIZE)
-    .sample(fraction=FRACTION)
     .select(COLUMNS)
     .pipe(
         sampling_strategy_wu2019,
@@ -102,38 +113,31 @@ df_train = (
         seed=123,
     )
     .pipe(create_binary_labels_column)
+    .sample(fraction=FRACTION)
 )
 # =>
 df_validation = (
     ebnerd_from_path(PATH.joinpath(DATASPLIT, "validation"), history_size=HISTORY_SIZE)
-    .sample(fraction=FRACTION)
     .select(COLUMNS)
     .pipe(create_binary_labels_column)
-)
-
-df_test = (
-    ebnerd_from_path(PATH.joinpath("ebnerd_testset", "test"), history_size=HISTORY_SIZE)
     .sample(fraction=FRACTION)
-    .with_columns(
-        pl.col(DEFAULT_INVIEW_ARTICLES_COL)
-        .list.first()
-        .alias(DEFAULT_CLICKED_ARTICLES_COL)
-    )
-    .select(COLUMNS)
-    .with_columns(
-        pl.col(DEFAULT_INVIEW_ARTICLES_COL)
-        .list.eval(pl.element() * 0)
-        .alias(DEFAULT_LABELS_COL)
-    )
 )
+df_train.head(2)
 
-# df_test = df_validation
-# df_train = df_train[:100]
-# df_validation = df_validation[:100]
-# df_test = df_test[:100]
-df_articles = pl.read_parquet(PATH.joinpath("articles.parquet"))
+# %% [markdown]
+# ## Load articles
 
-# =>
+# %%
+df_articles = pl.read_parquet(PATH.joinpath("ebnerd_demo/articles.parquet"))
+df_articles.head(2)
+
+# %% [markdown]
+# ## Init model using HuggingFace's tokenizer and wordembedding
+# In the original implementation, they use the GloVe embeddings and tokenizer. To get going fast, we'll use a multilingual LLM from Hugging Face. 
+# Utilizing the tokenizer to tokenize the articles and the word-embedding to init NRMS.
+# 
+
+# %%
 TRANSFORMER_MODEL_NAME = "FacebookAI/xlm-roberta-base"
 TEXT_COLUMNS_TO_USE = [DEFAULT_SUBTITLE_COL, DEFAULT_TITLE_COL]
 MAX_TITLE_LENGTH = 30
@@ -142,6 +146,7 @@ MAX_TITLE_LENGTH = 30
 transformer_model = AutoModel.from_pretrained(TRANSFORMER_MODEL_NAME)
 transformer_tokenizer = AutoTokenizer.from_pretrained(TRANSFORMER_MODEL_NAME)
 
+# We'll init the word embeddings using the
 word2vec_embedding = get_transformers_word_embeddings(transformer_model)
 #
 df_articles, cat_cal = concat_str_columns(df_articles, columns=TEXT_COLUMNS_TO_USE)
@@ -153,7 +158,11 @@ article_mapping = create_article_id_to_value_mapping(
     df=df_articles, value_col=token_col_title
 )
 
-# =>
+# %% [markdown]
+# # Initiate the dataloaders
+# In the implementations we have disconnected the models and data. Hence, you should built a dataloader that fits your needs.
+
+# %%
 train_dataloader = NRMSDataLoader(
     behaviors=df_train,
     article_dict=article_mapping,
@@ -168,26 +177,108 @@ val_dataloader = NRMSDataLoader(
     unknown_representation="zeros",
     history_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
     eval_mode=True,
-    batch_size=32,
+    batch_size=16,
 )
-test_dataloader = NRMSDataLoader(
-    behaviors=df_test,
-    article_dict=article_mapping,
-    unknown_representation="zeros",
-    history_column=DEFAULT_HISTORY_ARTICLE_ID_COL,
-    eval_mode=True,
-    batch_size=32,
-)
+
+# %% [markdown]
+# ## Train the model
+# 
+
+# %%
+import torch
+import torch.nn as nn
+epoch = 0
+num_epochs = 100
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f">> Using device: {device}")
+from ebrec.models.newsrec_pytorch import NRMSModel
+from torch.utils.tensorboard import SummaryWriter
+
+
+word2vec_embedding = torch.tensor(word2vec_embedding, dtype=torch.float32).to(device)
+print(word2vec_embedding.shape)
+nrms = NRMSModel(hparams_nrms=hparams_nrms, word2vec_embedding=word2vec_embedding, seed=42)
+print(nrms)
+optimizer = torch.optim.Adam(nrms.parameters(), lr=1e-3)
+loss_fn = nn.BCEWithLogitsLoss()
+writer = SummaryWriter("./logs")
+
+# Training loop
+num_epochs = 100
+for epoch in range(num_epochs):
+    nrms.train()  # Set the model to training mode
+    running_loss = 0.0
+    for i, ((his_input_title, pred_input_title), labels) in enumerate(train_dataloader):
+        his_input_title = torch.tensor(his_input_title, dtype=torch.long).to(device)
+        pred_input_title = torch.tensor(pred_input_title, dtype=torch.long).to(device)
+        labels = torch.tensor(labels, dtype=torch.long).to(device)
+        labels = labels.view(-1)
+        print("Input shape for user encoder:", his_input_title.shape)
+        print("Input shape for news encoder:", pred_input_title.shape)
+        optimizer.zero_grad()  # Zero the gradients
+        outputs = nrms(his_input_title, pred_input_title)  # Forward pass
+        loss = loss_fn(outputs.view(-1), labels.float())  # Compute the loss
+        loss.backward()  # Backward pass
+        optimizer.step()  # Update the parameters
+
+        running_loss += loss.item()
+        if i % 100 == 99:  # Print every 100 mini-batches
+            print(f"[{epoch + 1}, {i + 1}] loss: {running_loss / 100:.3f}")
+            running_loss = 0.0
+            
+    # Validation
+    nrms.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for (his_input_title, pred_input_title), labels in val_dataloader:
+            his_input_title = torch.tensor(his_input_title, dtype=torch.long).to(device)
+            pred_input_title = torch.tensor(pred_input_title, dtype=torch.long).to(device)
+            
+            labels = torch.tensor(labels, dtype=torch.long).to(device)
+            labels = labels.view(-1)
+            
+            outputs = nrms(his_input_title, pred_input_title)
+            loss = loss_fn(outputs.view(-1), labels.float())
+            val_loss += loss.item()
+            
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            
+    val_loss /= len(val_dataloader)
+    accuracy = 100 * correct / total
+    print(f"Epoch {epoch + 1}, Loss: {val_loss:.3f}, Accuracy: {accuracy:.2f}%")
+    
+       # Log to TensorBoard
+    writer.add_scalar("Loss/train", running_loss, epoch)
+    writer.add_scalar("Loss/validation", val_loss, epoch)
+    writer.add_scalar("Accuracy/validation", accuracy, epoch)
+
+# Save the model weights
+MODEL_NAME = "NRMS"
+MODEL_WEIGHTS = f"downloads/data/state_dict/{MODEL_NAME}/weights"
+torch.save(nrms.state_dict(), MODEL_WEIGHTS)
+            
+        
+    
+
+# %%
+MODEL_NAME = "NRMS"
+LOG_DIR = f"downloads/runs/{MODEL_NAME}"
+MODEL_WEIGHTS = f"downloads/data/state_dict/{MODEL_NAME}/weights"
 
 # CALLBACKS
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1)
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
-modelcheckpoint = tf.keras.callbacks.ModelCheckpoint(
-    filepath=MODEL_WEIGHTS, save_best_only=True, save_weights_only=True, verbose=1
-)
-
+# tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=LOG_DIR, histogram_freq=1)
+# early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=2)
+# modelcheckpoint = tf.keras.callbacks.ModelCheckpoint(
+#     filepath=MODEL_WEIGHTS, save_best_only=True, save_weights_only=True, verbose=1
+# )
 
 hparams_nrms.history_size = HISTORY_SIZE
+
+
 model = NRMSModel(
     hparams=hparams_nrms,
     word2vec_embedding=word2vec_embedding,
@@ -196,46 +287,60 @@ model = NRMSModel(
 hist = model.model.fit(
     train_dataloader,
     validation_data=val_dataloader,
-    epochs=EPOCHS,
-    callbacks=[tensorboard_callback, early_stopping],
+    epochs=1,
+    # callbacks=[tensorboard_callback, early_stopping, modelcheckpoint],
 )
-del (
-    transformer_tokenizer,
-    transformer_model,
-    train_dataloader,
-    val_dataloader,
-    df_validation,
-    df_train,
-    df_test,
+# Uncomment the following line if you have pre-trained weights
+# _ = model.model.load_weights(filepath=MODEL_WEIGHTS)
+
+# %% [markdown]
+# # Example how to compute some metrics:
+
+# %%
+pred_validation = model.scorer.predict(val_dataloader)
+
+# %% [markdown]
+# ## Add the predictions to the dataframe
+
+# %%
+df_validation = add_prediction_scores(df_validation, pred_validation.tolist()).pipe(
+    add_known_user_column, known_users=df_train[DEFAULT_USER_COL]
 )
-gc.collect()
+df_validation.head(2)
 
-print("saving model...")
-model.model.save_weights(MODEL_WEIGHTS)
-#
-print("loading model...")
-model.model.load_weights(MODEL_WEIGHTS)
+# %% [markdown]
+# ### Compute metrics
 
-# =>
-pred_test = model.scorer.predict(test_dataloader)
-df_test = add_prediction_scores(df_test, pred_test.tolist())
+# %%
+metrics = MetricEvaluator(
+    labels=df_validation["labels"].to_list(),
+    predictions=df_validation["scores"].to_list(),
+    metric_functions=[AucScore(), MrrScore(), NdcgScore(k=5), NdcgScore(k=10)],
+)
+metrics.evaluate()
 
-# metrics = MetricEvaluator(
-#     labels=df_validation["labels"].to_list(),
-#     predictions=df_validation["scores"].to_list(),
-#     metric_functions=[AucScore(), MrrScore(), NdcgScore(k=5), NdcgScore(k=10)],
-# )
-# metrics.evaluate()
+# %% [markdown]
+# ## Make submission file
 
-df_test = df_test.with_columns(
+# %%
+df_validation = df_validation.with_columns(
     pl.col("scores")
     .map_elements(lambda x: list(rank_predictions_by_score(x)))
     .alias("ranked_scores")
 )
+df_validation.head(2)
 
+# %% [markdown]
+# This is using the validation, simply add the testset to your flow.
+
+# %%
 write_submission_file(
-    impression_ids=df_test[DEFAULT_IMPRESSION_ID_COL],
-    prediction_scores=df_test["ranked_scores"],
-    path=DUMP_DIR.joinpath("predictions.txt"),
-    filename_zip=f"{DATASPLIT}_predictions-{DT_NOW}.zip",
+    impression_ids=df_validation[DEFAULT_IMPRESSION_ID_COL],
+    prediction_scores=df_validation["ranked_scores"],
+    path="downloads/predictions.txt",
 )
+
+# %% [markdown]
+# # DONE 🚀
+
+
--- src/ebrec/models/newsrec_pytorch/NRMSModel.py
+++ src/ebrec/models/newsrec_pytorch/NRMSModel.py
@@ -14,14 +14,21 @@ class NRMSModel(nn.Module):
         self.hparams_nrms = hparams_nrms
     
     def forward(self, his_input_title, pred_input_title):
-        _, _, title_size = pred_input_title.shape
+        batch_size, npratio, title_size = pred_input_title.shape
+        
         user_present = self.user_encoder(his_input_title)
+        user_present_unsqueezed = user_present.unsqueeze(1)
         pred_input_title_flat = pred_input_title.view(-1, title_size)
         news_present = self.candidate_encoder(pred_input_title_flat)
+        news_present = news_present.view(batch_size, npratio, -1)
+        print("User present shape: ", user_present_unsqueezed.shape)
+        print("News present shape: ", news_present.shape)
         
         # apply inner product between user_present and news_present
-        preds = torch.matmul(user_present, news_present.t())
+        preds = torch.matmul(user_present_unsqueezed, news_present.transpose(1, 2)).squeeze(1)
+        print("Preds after fot shape: ", preds.shape)
         # apply softmax to get probability
         preds = torch.softmax(preds, dim=1)
+        print("Preds shape: ", preds.shape)
 
         return preds
