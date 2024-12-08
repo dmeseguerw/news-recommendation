f4651430a63efc01952e8c46c93a657c62d52ee5
laura3668 <s233668@dtu.dk>
2024-12-08 19:39:45 +0100

    changed script to non default queue and 4 cores

--- jobscript.sh
+++ jobscript.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ### General options
 ### –- specify queue --
-#BSUB -q gpuv100
+#BSUB -q gpua40
 ### -- set the job Name --
 #BSUB -J testjob
 ### -- ask for number of cores (default: 1) --
