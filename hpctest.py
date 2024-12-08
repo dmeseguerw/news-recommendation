5e0f60231066f9bf3a814f3631fcb5fef49ea312
laura3668 <s233668@dtu.dk>
2024-12-08 23:09:08 +0100

    hpc scripts updated to be dynamic

--- hpctest.py
+++ hpctest.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-uno = 1
-dos = 2
+import argparse
 
-result = uno + dos
+# Argument parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("uno", type=int)
+parser.add_argument("dos", type=int)
+
+args = parser.parse_args()
+
+# Add parsed values
+result = args.uno + args.dos
 
 with open('outputtest.txt', 'w') as f:
     f.write(str(result))
